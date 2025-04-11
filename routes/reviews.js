b772d5650ea1771ade5c08084f74735f0f471b4c
Hania Flores <hanniaflores03@gmail.com>
2025-04-10 19:27:09 -0600

    adding authentication to reviews

--- routes/reviews.js
+++ routes/reviews.js
@@ -2,11 +2,12 @@ const router = require('express').Router();
 
 const reviewsController = require('../controllers/reviews');
 const { validateReview, validate } = require('../middlewares/reviewValidator');
+const {isAuthenticated} = require('../middlewares/authentication');
 
 router.get('/', reviewsController.getAll);
 router.get('/:id', validateReview(), validate, reviewsController.getSingle);
-router.post('/', validateReview(), validate, reviewsController.createReview);
-router.put('/:id', validateReview(), validate, reviewsController.updateReview);
-router.delete('/:id', reviewsController.deleteReview);
+router.post('/', isAuthenticated, validateReview(), validate, reviewsController.createReview);
+router.put('/:id', isAuthenticated, validateReview(), validate, reviewsController.updateReview);
+router.delete('/:id', isAuthenticated, reviewsController.deleteReview);
 
 module.exports = router;
